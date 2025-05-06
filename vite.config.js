a7cd40e91afee3ca20d9688d85da2d54bf7c27c9
Kobz <thomas.kobyluch@gmail.com>
2025-05-06 23:36:53 +0200

    updated ghpage references

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fusion-selector/', // <- use your repo name here
+  base: '/magic-survivor-fusion/',
 })
